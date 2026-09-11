0e4509aff7da2148f0c0021e6cdb5183ab923d99
RAJAT RAI <rairajat280@gmail.com>
2026-09-11 11:20:17 +0530

    Time: 5 ms (82.80%) | Memory: 46.1 MB (69.07%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<digits.length;i++){
+
+            if(digits[i]==0){
+                continue;
+            }
+            for(int j=0;j<digits.length;j++){
+
+                if(i==j){
+                    continue;
+                }
+                for(int k=0;k<digits.length;k++){
+
+                    if( j==k||k==i){
+                        continue;
+                    }
+                    if (digits[k] % 2 != 0) {
+                        continue;
+                    }
+
+                    int number = digits[i] * 100
+                               + digits[j] * 10
+                               + digits[k];
+
+                    set.add(number);
+
+
+
+                }
+            }
+        }
+        return set.size();
+
+
+
+        
+    }
+}
